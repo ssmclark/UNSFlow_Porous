c692470aa2414bb1931ccc5694a9fe943d1f6b3d
ssmclark <117084137+ssmclark@users.noreply.github.com>
2024-02-20 18:20:19 +0000

    Update simRun.jl

--- Airfoils/steadyAirfoilPorous/simRun.jl
+++ Airfoils/steadyAirfoilPorous/simRun.jl
@@ -1,10 +1,11 @@
-push!(LOAD_PATH,"UnsteadyFlowSolvers.jl")
+push!(LOAD_PATH,"C:\\Users\\user\\ENG_Project_4\\UnsteadyFlowSolvers.jl\\UnsteadyFlowSolvers.jl")
 
 using UnsteadyFlowSolvers
+using Plots
 
-alphadef = ConstDef(5. *pi/180)
+alphadef = ConstDef(0. *pi/180)
 
-hdef = ConstDef(5.)
+hdef = ConstDef(0.)
 
 udef = ConstDef(1.)
 
@@ -13,28 +14,30 @@ full_kinem = KinemDef(alphadef, hdef, udef)
 pvt = 0.25
 
 geometry = "FlatPlate"
-surf = TwoDSurfPorous(geometry, pvt, full_kinem, rho = 0.02, rho_e = 0, psi = 0)
+
+surf = TwoDSurfPorous(geometry, pvt, full_kinem, [100.0], rho = 0.02, rho_e = 1.2, phi = 5.1)
+surf2 = TwoDSurf(geometry, pvt, full_kinem, [100.0], rho = 0.02)
 
 curfield = TwoDFlowField()
 
-dtstar = find_tstep(alphadef)
+dtstar = 0.015
 
-t_tot = 10.
+t_tot = 5.
 
-nsteps = Int(round(t_tot/dtstar))+1
+nsteps = 1000  #Int(round(t_tot/dtstar))+1
 
 startflag = 0
 
 writeflag = 1
 
-writeInterval = t_tot/10.
+writeInterval = t_tot/5.
 
-delvort = delnone()
+#delvort = delSpalart(500, 12, 1e-5)
+delvort = delNone()
 
-mat, surf, curfield = ldvm2DOFPorous(surf, curfield, nsteps dtstar, startflag, writeflag, writeInterval,delvort, wakerollup=1)
+mat, surf, curfield, p_com = ldvmLin(surf, curfield, nsteps, dtstar,startflag, writeflag, writeInterval, delvort)
 
-makeForcePlots2D()
+#plot(surf_p.x, surf_p.ws)
 
-makeVortPlots2D()
 
 cleanWrite()
