01c174fc8bc4d8bb24593c5d1ac4f32d5f6b46cd
ssmclark <seanclark778@gmail.com>
2024-03-04 15:48:27 +0000

    Added PlotlyJS backend

--- Airfoils/steadyAirfoilPorous/simRun.jl
+++ Airfoils/steadyAirfoilPorous/simRun.jl
@@ -1,8 +1,10 @@
-push!(LOAD_PATH,"C:\\Users\\user\\ENG_Project_4\\UNSFlow_Porous\\UnsteadyFlowSolvers.jl\\src\\UnsteadyFlowSolvers.jl")
+#push!(LOAD_PATH,"path\\to\\UnsteadyFlowSolvers.jl\\src")
+push!(LOAD_PATH,"C:\\Users\\user\\ENG_Project_4\\UNSFlow_Porous\\UnsteadyFlowSolvers.jl\\src\\")
 
 using UnsteadyFlowSolvers
+using PlotlyJS
 
-alphadef = ConstDef(0. *pi/180)
+alphadef = ConstDef(1. *pi/180)
 
 hdef = ConstDef(0.)
 
@@ -10,10 +12,10 @@ udef = ConstDef(1.)
 
 full_kinem = KinemDef(alphadef, hdef, udef)
 
-pvt = 0.25
+pvt = 0.2
 
 geometry = "FlatPlate"
-#testgit
+
 surf = TwoDSurfPorous(geometry, pvt, full_kinem, [100.0], rho = 0.02, rho_e = 1.2, phi = 5.1)
 surf2 = TwoDSurf(geometry, pvt, full_kinem, [100.0], rho = 0.02)
 
@@ -36,7 +38,6 @@ delvort = delNone()
 
 mat, surf, curfield, p_com = ldvmLin(surf, curfield, nsteps, dtstar,startflag, writeflag, writeInterval, delvort)
 
-#plot(surf.x, p_com)
-
-
 cleanWrite()
+
+plot(surf.x, p_com)
